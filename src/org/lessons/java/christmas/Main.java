40998bc830fd48e71164a528bc5f1b93a54886fe
DarioGiusino <dario.giusino@gmail.com>
2023-05-12 11:48:37 +0200

    solved ex

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,5 +1,30 @@
 package org.lessons.java.christmas;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		List<String> wishList = new ArrayList<>();
+
+		while (true) {
+			System.out.print("Vuoi inserire un nuovo desiderio: ");
+			Boolean bool = sc.nextBoolean();
+
+			if (bool) {
+				System.out.println("Scrivi il desiderio...");
+				String wish = sc.next();
+				wishList.add(wish);
+				System.out.println("La lista dei tuoi desideri contiene " + wishList.size()
+						+ " elemento/i. Vuoi continuare ad inserirne altri?");
+			} else {
+				System.out.println("Grazie e arrivederci!");
+				return;
+			}
+		}
+	}
 }
